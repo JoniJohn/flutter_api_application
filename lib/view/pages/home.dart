3c3acda756b7cfea004edc555b2c7eb65a0a45aa
JoniJohn <whosekid@gmail.com>
2021-10-01 14:16:53 +0200

    Bottom App Bar

--- lib/view/pages/home.dart
+++ lib/view/pages/home.dart
@@ -8,6 +8,7 @@ class Home extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        leading: IconButton(onPressed: () {}, icon: const Icon(Ionicons.menu)),
         title: const Text('API App'),
         actions: [
           IconButton(
@@ -15,6 +16,34 @@ class Home extends StatelessWidget {
             icon: const Icon(Ionicons.cart),
           ),
         ],
+        bottom: PreferredSize(
+          child: SizedBox(
+            height: 54.0,
+            // color: Colors.red,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Text('Shopping App'),
+                  Row(
+                    children: [
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Ionicons.list),
+                      ),
+                      IconButton(
+                        onPressed: () {},
+                        icon: const Icon(Ionicons.grid),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+          preferredSize: const Size.fromHeight(54.0),
+        ),
       ),
       body: const Center(
         child: Text('Some API data'),
